ab0864fd62143e98fee3e835599ba26a3083e1ee
shyam <paulshyam546@gmail.com>
2021-07-03 18:09:49 +0530

    navbar sticky top

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -14,7 +14,7 @@ function Navbar(props){
 
 
     return(
-        <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
         <a className="navbar-brand" href="">Instaclone</a>
        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
